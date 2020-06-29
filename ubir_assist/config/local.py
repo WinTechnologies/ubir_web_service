9b31a6180dc5d6588d90c4817bb78d28237aa9a4
GuangZhe Li <win.tech0801@gmail.com>
2020-06-29 18:45:55 +0800

    add timeout option for sqlite3

--- ubir_assist/config/local.py
+++ ubir_assist/config/local.py
@@ -20,6 +20,9 @@ class Local(Common):
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'OPTIONS': {
+                'timeout': 30,
+            }
         }
     }
     # Testing
--- ubir_assist/config/production.py
+++ ubir_assist/config/production.py
@@ -20,6 +20,9 @@ class Production(Common):
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'OPTIONS': {
+                'timeout': 30,
+            }
         }
     }
 
