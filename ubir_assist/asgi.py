2a90624471fab4a4462ae5f46b766e3fb8011a94
GuangZhe Li <win.tech0801@gmail.com>
2020-07-22 02:16:15 +0800

    add asgi

--- ubir_assist/asgi.py
+++ ubir_assist/asgi.py
@@ -4,10 +4,13 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubir_assist.config")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
-django.setup()
+if os.getenv('DJANGO_ENV') == 'production':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubir_assist.config")
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubir_assist.config")
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+
 application = get_default_application()
